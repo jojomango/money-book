4ab3091643c932eabcc300bc9c49f5853cfa507c
mingchiao.wu <q9898989@gmail.com>
2021-06-19 11:53:01 +0800

    add font and loading

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as Font from 'expo-font';
+import Apploading from 'expo-app-loading';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import transReducer from './store/reducers/transactions';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+}
 
 const rootReducer = combineReducers({
   transactions: transReducer,
@@ -17,8 +26,19 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  if (!fontLoaded) {
+    return (
+      <Apploading
+        startAsync={() => fetchFonts()}
+        onFinish={() => {setFontLoaded(true)}}
+        onError={console.warn}
+      />
+    );
+  }
 
   if (!isLoadingComplete) {
     return null;
